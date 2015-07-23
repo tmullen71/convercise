f9dc61bdf01d4d45bf8e465e3b2453e165806362
Mikołaj Stolarski <grimor.mc@gmail.com>
2015-07-23 23:36:04 +0200

    Fixed URL's for Heroku

--- public/modules/core/services/thisExch.client.service.js
+++ public/modules/core/services/thisExch.client.service.js
@@ -68,7 +68,7 @@ angular.module('core').service('ThisExch', ['Authentication', '$http',
       };
 
       var updateDoneTask = function(iCopy){
-        $http.put('http://localhost:3000/doneTasks/'+this_exch.doneTaskIds[iCopy], exch_data.doneTasks[iCopy])
+        $http.put('http://cv-audio-rec1.herokuapp.com/doneTasks/'+this_exch.doneTaskIds[iCopy], exch_data.doneTasks[iCopy])
         .success(function(data){
           console.log('Updated doneTask:' + data);
         });
@@ -89,7 +89,7 @@ angular.module('core').service('ThisExch', ['Authentication', '$http',
         };
       }
 
-      $http.put('http://localhost:3000/exchanges/'+this_exch.id, exchUpdate)
+      $http.put('http://cv-audio-rec1.herokuapp.com/exchanges/'+this_exch.id, exchUpdate)
         .success(function(data){
           console.log('Updated Exchange');
           console.log(data);
@@ -112,7 +112,7 @@ angular.module('core').service('ThisExch', ['Authentication', '$http',
       }
 
       console.log(exchUpdate);
-      $http.put('http://localhost:3000/exchanges/'+this_exch.id, exchUpdate)
+      $http.put('http://cv-audio-rec1.herokuapp.com/exchanges/'+this_exch.id, exchUpdate)
         .success(function(data){
           console.log('Updated Exchange');
           console.log(data);
